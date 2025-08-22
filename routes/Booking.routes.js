29cb000926405908ce278129dc3fc20667ae2576
FERNANDO JACINTO DA SILVA <fernando.jsilva9@senacsp.edu.br>
2025-08-21 22:23:37 -0300

    added routes/booking.routes and booking.controller

---
+++ routes/Booking.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+// localhost:3335/bookings/113131313131/spots
+
+// router.post('/:spot_id/spots', BookingController.store);
+
+module.exports = router;
