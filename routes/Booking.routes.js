f5615a17e440c138b6974fa9e8987ca11e3db162
FERNANDO JACINTO DA SILVA <fernando.jsilva9@senacsp.edu.br>
2025-08-27 22:07:27 -0300

    added model/Booking, routes/Booking.routes and controllers/Booking.controller[ store(), storeApproval(), storeRejection() ]

--- routes/Booking.routes.js
+++ routes/Booking.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const BookingController = require('../controllers/Booking.controller')
 
 // localhost:3335/bookings/113131313131/spots
 
-// router.post('/:spot_id/spots', BookingController.store);
+router.post('/:spot_id/spots', BookingController.store);
+router.post('/:booking_id/approvals', BookingController.storeApproval)
+router.post('/:booking_id/rejections', BookingController.storeRejection)
 
 module.exports = router;
